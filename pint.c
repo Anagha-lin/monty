919cd3c413121d56490f5edf32eb47b92618eefe
Anagha-lin <linusanaghamay12.com>
2023-11-17 09:58:12 -0800

    a

---
+++ pint.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * my_pint - Prints the value at the top of the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number of the opcode.
+ *
+ * Description: This function prints the integer value stored at the top
+ * of the stack. If the stack is empty, it prints an error message and exits.
+ */
+void my_pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	free(glob.line);
+	free_stack(*stack);
+	exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}

8ed605b7e61c88ca0c2adec6de73e1a7dbc03fd9
Anagha-lin <linusanaghamay12.com>
2023-11-17 10:39:58 -0800

    a

--- pall.c
+++ pall.c
@@ -1,22 +1,76 @@
-#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * my_pall - Print all values on the stack.
- * @stack: Pointer to the top of the stack.
- * @line_number: Line number of the opcode.
- *
- * Description: This function prints all the integer values stored
- * in the nodes of the stack.
- */
-void my_pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current = *stack;
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
 
-	while (current != NULL)
-	{
-	printf("%d\n", current->n);
-	current = current->next;
-	(void)line_number;
-	}
+typedef struct Stack {
+    Node* top;
+} Stack;
+
+Stack* createStack();
+void push(Stack* stack, int value);
+void pall(Stack* stack);
+void freeStack(Stack* stack);
+
+int main(void) {
+    Stack* stack = createStack();
+
+   
+    push(stack, 10);
+    push(stack, 20);
+    push(stack, 30);
+    pall(stack);
+
+    freeStack(stack);
+
+    return 0;
+}
+
+Stack* createStack() {
+    Stack* stack = malloc(sizeof(Stack));
+    if (stack == NULL) {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    stack->top = NULL;
+
+    return stack;
+}
+
+void push(Stack* stack, int value) {
+    Node* newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->data = value;
+    newNode->next = stack->top;
+    stack->top = newNode;
+}
+
+void pall(Stack* stack) {
+    Node* current = stack->top;
+
+    while (current != NULL) {
+        printf("%d\n", current->data);
+        current = current->next;
+    }
+}
+
+void freeStack(Stack* stack) {
+    Node* current = stack->top;
+
+    while (current != NULL) {
+        Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    free(stack);
 }
 

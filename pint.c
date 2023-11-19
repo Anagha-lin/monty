dfee15c0a38d2ac70baf6837398de4b5f880f7f9
Anagha-lin <linusanaghamay12.com>
2023-11-19 10:15:43 -0800

    a

--- pint.c
+++ pint.c
@@ -11,15 +11,15 @@
  * failure status.
  */
 void my_pint(stack_t **stack, unsigned int line_number)
-	if (*stack == NULL)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		free(glob.line);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	free(glob.line);
+	free_stack(*stack);
+	exit(EXIT_FAILURE);
 	}
 
 	printf("%d\n", (*stack)->n);
 }
+

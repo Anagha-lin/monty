b2dcbf28d98a7370e58f7e469d8dcaa49478cd1c
Anagha-lin <linusanagha37@gmail.com>
2024-02-17 12:31:42 -0800

    a

--- main.c
+++ main.c
@@ -2,19 +2,16 @@
 
 glob_t glob;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
+    if (argc != 2) {
+        fprintf(stderr, "Usage: monty file\n");
         exit(EXIT_FAILURE);
     }
 
     glob.file = fopen(argv[1], "r");
-    if (glob.file == NULL)
-    {
+    if (glob.file == NULL) {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
--- monty.h
+++ monty.h
@@ -1,56 +1,43 @@
-#ifndef MONTY_H
-#define MONTY_H
-#define _GNU_SOURCE
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-/**
- * struct stack_s - Doubly linked list representation of a stack (or queue)
- * @n: Integer value stored in the node
- * @prev: Points to the previous element of the stack (or queue)
- * @next: Points to the next element of the stack (or queue)
- *
- * Description: Doubly linked list node structure for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
+truct stack_s - Doubly linked list representation of a stack (or queue)
+	 * @n: Integer value stored in the node
+	 *  * @prev: Points to the previous element of the stack (or queue)
+	 *   * @next: Points to the next element of the stack (or queue)
+	 *    *
+	 *     * Description: Doubly linked list node structure for stack, queues, LIFO, FIFO
+	 *      */
+	 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+	    int n;
+	        struct stack_s *prev;
+		    struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - Opcode and its function
- * @opcode: The opcode
- * @f: Function to handle the opcode
- *
- * Description: Opcode and its function for stack, queues, LIFO, FIFO
- */
+ *  * struct instruction_s - Opcode and its function
+ *   * @opcode: The opcode
+ *    * @f: Function to handle the opcode
+ *     *
+ *      * Description: Opcode and its function for stack, queues, LIFO, FIFO
+ *       */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+	    char *opcode;
+	        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
- * struct global_s - variables -> args, file, line content
- * @arg: value
- * @line: input line content
- * @file: pointer to monty file
- *
- * Description: variables that carries values through the program
- */
+ *  * struct global_s - variables -> args, file, line content
+ *   * @arg: value
+ *    * @line: input line content
+ *     * @file: pointer to monty file
+ *      *
+ *       * Description: variables that carries values through the program
+ *        */
 typedef struct global_s
 {
-	char *arg;
-	FILE *file;
-	char *line;
+	    char *arg;
+	        FILE *file;
+		    char *line;
 } glob_t;
 
 extern glob_t glob;
@@ -77,5 +64,5 @@ stack_t *add_node(stack_t **stack, const int n);
 int is_number(char *str);
 void free_stack(stack_t *stack);
 
-#endif /* monty.h */
+#endif /* MONTY_H */
 
